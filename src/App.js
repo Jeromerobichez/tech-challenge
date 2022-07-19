ac297548f271132648103f45722f36ca0c793ab8
jeromerobichez <jerome.robichez@gmail.com>
2022-07-19 10:36:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,34 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className='App-header'>
+  <h1>
+    <img className='logo-wild' src="https://www.wildcodeschool.com/assets/logo_main-e4f3f744c8e717f1b7df3858dce55a86c63d4766d5d9a7f454250145f097c2fe.png" alt="Wild Code School logo" />
+    Les Argonautes
+  </h1>
+</header>
+<main>
+  
+
+  <h2>Ajouter un(e) Argonaute</h2>
+  <form class="new-member-form">
+    <label for="name">Nom de l&apos;Argonaute</label>
+    <input id="name" name="name" type="text" placeholder="Charalampos" />
+    <button type="submit">Envoyer</button>
+  </form>
+  
+
+  <h2>Membres de l'équipage</h2>
+  <section class="member-list">
+    <div class="member-item">Eleftheria</div>
+    <div class="member-item">Gennadios</div>
+    <div class="member-item">Lysimachos</div>
+  </section>
+</main>
+
+<footer>
+  <p>Réalisé par Jason en Anthestérion de l'an 515 avant JC</p>
+</footer>
     </div>
   );
 }
